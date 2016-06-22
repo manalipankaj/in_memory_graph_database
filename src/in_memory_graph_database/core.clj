ef114169a91126d7f8fcea9afea94a80a6c48642
Pankaj <pankaj.manali@gmail.com>
2016-06-22 23:49:21 +0530

    Changed to reff

--- src/in_memory_graph_database/core.clj
+++ src/in_memory_graph_database/core.clj
@@ -3,7 +3,6 @@
    (require [cheshire.core :as json]
            [cheshire.parse :as parse]))
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defrecord Vertices
     [id label in out data])
@@ -11,9 +10,9 @@
 (defrecord Edges
     [label in out data])
 
-(def vertex-index (atom {}))
+(def vertex-index (ref {}))
 
-(def edges-array (atom []))
+(def edges-array (ref []))
 
 (defn generate-id
   "Generates unique ID"
@@ -21,10 +20,11 @@
   (. clojure.lang.RT (nextID)))
 
 (defn add-vertex-index
-  [id {:keys [id label data]}]
-  (if (contains? @vertex-index id)
+  [ids {:keys [id label data]}]
+  (if (contains? @vertex-index ids)
     nil
-    (swap! vertex-index assoc id (Vertices. id label nil nil data))))
+    (dosync 
+     (alter vertex-index assoc ids (Vertices. ids label nil nil data)))))
 
 (defn add-vertex
   [vertex]
@@ -40,24 +40,28 @@
   "Creates uni realtionship edge"
   [in out edge]
   (let [in-node (get @vertex-index in) out-node (get @vertex-index out)] 
-    (swap! vertex-index assoc 
-           in (Vertices. (:id in-node) (:label in-node) (conj (:in in-node) edge) (:out in-node) (:data in-node)))
-    (swap! vertex-index assoc
-           out (Vertices. (:id out-node) (:label out-node) (:in out-node) (conj (:out out-node) edge) (:data out-node)))))
+    (dosync 
+     (alter vertex-index assoc 
+            in (Vertices. (:id in-node) (:label in-node) (conj (:in in-node) edge) (:out in-node) (:data in-node))))
+    (dosync 
+     (alter vertex-index assoc
+            out (Vertices. (:id out-node) (:label out-node) (:in out-node) (conj (:out out-node) edge) (:data out-node))))))
 
 (defn add-bidirectional-relation
   [in out edge edge2]
   (let [in-node (get @vertex-index in) out-node (get @vertex-index out)] 
-    (swap! vertex-index assoc 
-           in (Vertices. (:id in-node) (:label in-node) (:in in-node) (conj (:out in-node) edge2) (:data in-node)))
-    (swap! vertex-index assoc
-           out (Vertices. (:id out-node) (:label out-node) (:in out-node) (conj (:out out-node) edge) (:data out-node)))))
+    (dosync 
+     (alter vertex-index assoc 
+            in (Vertices. (:id in-node) (:label in-node) (:in in-node) (conj (:out in-node) edge2) (:data in-node))))
+    (dosync 
+     (alter vertex-index assoc
+            out (Vertices. (:id out-node) (:label out-node) (:in out-node) (conj (:out out-node) edge) (:data out-node))))))
 
 (defn add-edge
   [{:keys [label in out data] :as node}]
   (when (and (contains? @vertex-index in) (contains? @vertex-index out))
     (let [edge (Edges. label in out data)] 
-      (swap! edges-array conj edge)
+      (dosync (alter edges-array conj edge))
       (cond 
        (contains? node :uni) (add-relation-to-vertex in out edge)
        :else (add-bidirectional-relation in out edge (Edges. label out in data))))))
@@ -69,8 +73,13 @@
 
 (defn delete-all
   []
-  (reset! vertex-index {})
-  (reset! edges-array []))
+  (dosync (ref-set vertex-index {}))
+  (dosync (ref-set edges-array [])))
+
+(defn delete-node
+  [node]
+  (dosync 
+   (alter @vertex-index dissoc node)))
 
 (defn show-node-data
   []
@@ -80,25 +89,33 @@
   []
   (println (json/generate-string @edges-array {:pretty true})))
 
+(defn show-node
+  [node]
+  (println (json/generate-string node {:pretty true})))
+
 (defn get-all-relationship
   []
   (println
-   (filter #(not (nil? %)) (map #(:label (first (:in (second %)))) @vertex-index))
-   (filter #(not (nil? %)) (map #(:label (first (:out (second %)))) @vertex-index))))
+   (set 
+    (concat 
+     (filter #(not (nil? %)) (map #(:label (first (:in (second %)))) @vertex-index))
+     (filter #(not (nil? %)) (map #(:label (first (:out (second %)))) @vertex-index))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;Query;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn get-next-node
   [node relation]
   (if (string? node)
     (filter #(= (:label %) relation) (:out (get @vertex-index node)))
-    (if (= (count node) 1) 
+    (if (= (count node) 1)
       (filter #(= (:label %) relation) (:out (get @vertex-index (:in (first node)))))
       (map (fn [x] (filter (fn [y] (= (:label y) relation)) (:out (get @vertex-index (:in x))))) node))))
 
 (def func-for-query-map 
-  (fn [x] (if (map? x) (:in x) (first (map (fn [y] (str (:out y) " "  (:label y) " " (:in y))) x)))))
+  (fn [x] (if (map? x) 
+            (get @vertex-index (:in x)) 
+            (first (map (fn [y]  {(:out y) (get @vertex-index (:in y))}) x)))))
 
 (defn query1
   [query]
-  (map func-for-query-map (reduce get-next-node query)))
+  (show-node (map func-for-query-map (reduce get-next-node query))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
